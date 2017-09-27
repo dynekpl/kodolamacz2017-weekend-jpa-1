d222c3ad89a19e363516fcb47c5aea4ef9b430ea
dynekpl <dynek82@gmail.com>
2017-09-27 18:37:22 +0200

    Preparation file fix

--- src/main/java/weekend/exercises/Preparation.java
+++ src/main/java/weekend/exercises/Preparation.java
@@ -4,25 +4,31 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Preparetion {
+public class Preparation {
 
     public static EntityManager entityManager;
+
+    public static final String PASSWORD = "tajne";
 
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("postgres");
         entityManager = entityManagerFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        Town town = new Town("Warszawa", 200);
-        addTown(town);
-        prepareAndAddUser("Marcin", "tajne", town);
+        Town town = prepareAndAddTown("Warszawa", 200);
+        prepareAndAddUser("Marcin", PASSWORD, town);
+        prepareAndAddUser("Kamil", PASSWORD, town);
 
+        town = prepareAndAddTown("Wroclaw", 300);
+        prepareAndAddUser("Mariusz", PASSWORD, town);
+        prepareAndAddUser("Tomek", PASSWORD, town);
 
-        //prepareAndAddTown("Wroclaw", 300);
-        //prepareAndAddTown("Gdansk", 400);
-        //prepareAndAddTown("Krakow", 500);
-        //prepareAndAddTown("Opole", 600);
+        town = prepareAndAddTown("Gdansk", 400);
+        prepareAndAddUser("Radek", PASSWORD, town);
 
+        town = prepareAndAddTown("Krakow", 500);
+
+        town = prepareAndAddTown("Opole", 600);
 
         entityManager.getTransaction().commit();
         entityManager.close();
@@ -34,15 +40,17 @@ public class Preparetion {
     }
 
     private static void prepareAndAddUser(String login, String password, Town town) {
-        new User(login, password, town);
+        User user = new User(login, password, town);
+        entityManager.persist(user);
     }
 
     private static void addTown(Town town) {
         entityManager.persist(town);
     }
 
-    private static void prepareAndAddTown(String name, int inhabitants) {
+    private static Town prepareAndAddTown(String name, int inhabitants) {
         Town town = new Town(name, inhabitants);
         addTown(town);
+        return town;
     }
 }
